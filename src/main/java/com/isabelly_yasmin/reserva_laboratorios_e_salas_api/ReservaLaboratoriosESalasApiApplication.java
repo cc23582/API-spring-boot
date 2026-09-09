9d4196cc710c5dc388a888ab341067d1729085fd
Isabelly Gomes de Oliveira <gomesdeoliveira445@gmail.com>
2026-09-09 11:00:12 -0300

    feat: configuracao inicial do projeto spring boot (reservas)

---
+++ src/main/java/com/isabelly_yasmin/reserva_laboratorios_e_salas_api/ReservaLaboratoriosESalasApiApplication.java
@@ -0,0 +1,13 @@
+package com.isabelly_yasmin.reserva_laboratorios_e_salas_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReservaLaboratoriosESalasApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReservaLaboratoriosESalasApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/isabelly_yasmin/reserva_laboratorios_e_salas_api/ReservaLaboratoriosESalasApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.isabelly_yasmin.reserva_laboratorios_e_salas_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReservaLaboratoriosESalasApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
